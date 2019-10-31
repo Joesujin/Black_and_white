3dee64407b65f33f54cd08a9149ef9435cdbd3b8
Joe Sujin <joesujin@gmail.com>
2019-10-31 23:00:52 +0530

    dificulty on notice done

---
+++ Assets/Scripts/ColorPairs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ColorPairs 
+{
+    public int Color1;
+    public int Color2;
+
+    public ColorPairs(int _c1,int _c2)
+    {
+        Color1 = _c1;
+        Color2 = _c2;
+
+    }
+}
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 [System.Serializable]
 public class GameState : MonoBehaviour
 {
     public List<Notices> notices = new List<Notices>();
-    public List< Project> projects = new List<Project>();
+    public List<Project> projects = new List<Project>();
+    public ColorPairs[] NoticeColors = new ColorPairs[15];
+    public List<ColorPairs> UsedPairs = new List<ColorPairs>();
 
     public Dictionary<int, Color> DefaultColors = new Dictionary<int, Color>();
     public Dictionary<int, Color> inGameColors = new Dictionary<int, Color>();
@@ -40,7 +43,7 @@ public class GameState : MonoBehaviour
     public int GameDay = 0;
     public int daySeconds = 15;
     public int noticeDay = 5;
-    public int difficulty=0;
+    public int difficulty = 0;
     public int Projectcap = 4;
     public int realdifficulty;
     public string DayOfWeek;
@@ -52,7 +55,8 @@ public class GameState : MonoBehaviour
     public int CoundownSeconds;
     public Text CountdownTextPs;
     public Text CountdownTextDs;
-    public Text StoryText;
+    //public Text StoryText;
+    public TextMeshProUGUI StoryText;
     public Text Projectdetails;
 
     public Color SelectedColor;
@@ -73,14 +77,55 @@ public class GameState : MonoBehaviour
         DefaultColors.Add(5, Green);
         DefaultColors.Add(6, Yellow);
 
-        foreach(int key in DefaultColors.Keys)
+        foreach (int key in DefaultColors.Keys)
         {
             inGameColors.Add(key, DefaultColors[key]);
         }
+        int k = 0;
+        for (int i = 1; i < 7; i++)
+        {
+            for (int j = 1; j < 7; j++)
+            {
+                if (i != j)
+                {
+                    bool pairExists = false;
+                    foreach (var colorPair in NoticeColors)
+                    {
+                        if (colorPair.Color1 == i)
+                        {
+                            if (colorPair.Color2 == j)
+                            {
+                                pairExists = true;
+                            }
+
+                        }
+                        if (colorPair.Color1 == j)
+                        {
+                            if (colorPair.Color2 == i)
+                            {
+                                pairExists = true;
+                            }
+
+                        }
+                    }
+                    if (pairExists == false)
+                    {
+                        NoticeColors[k].Color1 = i;
+                        NoticeColors[k].Color2 = j;
+                        k++;
+                    }
+
+                }
+
+
+            }
+        }
+
 
         UpdateStory(GameDay);
 
         projects.Clear();
+        UsedPairs.Clear();
 
     }
 
@@ -100,7 +145,12 @@ public class GameState : MonoBehaviour
 
     public void EndDay()
     {
-        if(CoundownSeconds > 1)
+        if(UsedPairs.Count == 3)
+        {
+            UsedPairs.Clear();
+        }
+        GameDay++;
+        if (CoundownSeconds > 1)
         {
             Debug.Log("EndDay buttonClick");
             difficulty++;
@@ -111,25 +161,25 @@ public class GameState : MonoBehaviour
             Events.saveButton(currentProject);
             ProjectScreenCall();
         }
+
+        StopAllCoroutines();
+
+        Score = 0;
+        foreach (Project project in projects)
+        {
+            bool _isPass = project.CrossCheck();
+            if (_isPass)
+            {
+                Score++;
+            }
+        }
+
+
         UpdateStory(GameDay);
         Events.ReportScreen();
-        StopAllCoroutines();
-        if (dayCount != GameDay)
-        {
-            Score = 0;
-            foreach (Project project in projects)
-            {
-                bool _isPass = project.CrossCheck();
-                if (_isPass)
-                {
-                    Score++;
-                }
-            }
-            GameDay++;
-        }
         gameObject.GetComponent<Life>().UpdateLifeStats();
         //GAME END STATE
-        if(gameObject.GetComponent<Life>().Money <= 0)
+        if (gameObject.GetComponent<Life>().Money <= 0)
         {
             UpdateStory(7);
             Events.EndGame();
@@ -138,12 +188,12 @@ public class GameState : MonoBehaviour
 
     public void RestartGame()
     {
-        SceneManager.LoadScene("7DaysScene",LoadSceneMode.Single);
+        SceneManager.LoadScene("7DaysScene", LoadSceneMode.Single);
     }
 
     private void Update()
     {
-        CountdownTextPs.text = CoundownSeconds.ToString(); 
+        CountdownTextPs.text = CoundownSeconds.ToString();
         CountdownTextDs.text = CoundownSeconds.ToString();
 
 
@@ -196,34 +246,47 @@ public class GameState : MonoBehaviour
         switch (Day)
         {
             case 0:
-                StoryText.text = "My Name is John i used to work here..." +
-                    "\n now you are gonna..." +
-                    "\n All the best Budd";
-                DayOfWeek = "Monday";
+                StoryText.text = "<size=200%>Dear applicant" +
+                    "\n<line-indent=15%><size=100%>This is your Probation period for the job application we recieved from you.</line-indent>" +
+                    "\n\n<u><b>Instructions are as follows.</u></b>" +
+                    "\n<line-indent=8%> -Find the <b><size=120%>Big Red Button<size=100%></b>" +
+                    "\n -Use the Colors Match the grid with the Job Sheet" +
+                    "\n -<i>Submit.</i>" +
+                    "\n -<color=#009500>Green tick</color> is good,<color=#950000> Red cross</color> is bad</line-indent>" +
+                    "\n\n<size=150%><i><b>All the best</i></b>";
+                DayOfWeek = "Day 1";
                 break;
             case 1:
-                StoryText.text = "You know the dril";
-                DayOfWeek = "Tuesday";
+                StoryText.text = "Well Done of your first day." +
+                    "\n you've done " + projects.Count.ToString() + " out of which " + Score.ToString() + "are Valid" +
+                    "\n\n you can checkout those projects whenever you like and correct them" +
+                    "\nKeep up the good work" +
+                    "\n oh and look out for the notices";
+                DayOfWeek = "Day 2";
                 break;
             case 2:
-                StoryText.text = "lets work out Some Deal okay???";
-                DayOfWeek = "Wednesday";
+                StoryText.text = "Notices are office memos that are passed down by the people above" +
+                    "\n they have no idea whats going on, But they always want to change the meaning of on thing to another" +
+                    "\n so we need to keep our projects up to date." +
+                    "\n\n you'll lose money for all the invalid projects you have.";
+                DayOfWeek = "Day 3";
                 break;
             case 3:
                 StoryText.text = "Story line 4";
-                DayOfWeek = "Thursday";
+                DayOfWeek = "Day 4";
                 break;
             case 4:
                 StoryText.text = "Story line 5";
-                DayOfWeek = "Friday";
+                DayOfWeek = "Day 5";
                 break;
             case 5:
+
                 StoryText.text = "Story line 6";
-                DayOfWeek = "Saturday";
+                DayOfWeek = "Day 6";
                 break;
             case 6:
                 StoryText.text = "Story line 7";
-                DayOfWeek = "Sunday";
+                DayOfWeek = "Day 7";
                 break;
             case 7:
                 StoryText.text = "Story line 8";
@@ -237,20 +300,20 @@ public class GameState : MonoBehaviour
         if (projects.Count < 20)
         {
 
-            
+
 
             realdifficulty = Mathf.Clamp(realdifficulty, 3, 7);
 
             if (projects.Count > Projectcap)
             {
-                if(realdifficulty < 7)
+                if (realdifficulty < 7)
                 {
                     realdifficulty++;
                 }
                 Projectcap += 3;
             }
-        
-            Project tempP = new Project(projectId,realdifficulty);
+
+            Project tempP = new Project(projectId, realdifficulty);
             currentProject = projectId;
             projects.Add(tempP);
             GameObject Button;
@@ -299,13 +362,13 @@ public class GameState : MonoBehaviour
     {
         currentProject = _currentProject;
         int[] data = projects[_currentProject].ReturnTiledata();
-        Events.recallProject(data,currentProject);
+        Events.recallProject(data, currentProject);
         Projectdetails.text = projects[_currentProject].ProjectDetails;
         Events.RecallDrawscreen();
         gameObject.GetComponent<TileMap>().LoadQuestion(projects[currentProject].QuestionData);
     }
- 
-    
+
+
     public void saveProject(int[] projectData)
     {
         projects[currentProject].UpdateTiledata(projectData);
@@ -315,15 +378,15 @@ public class GameState : MonoBehaviour
     {
         return (projects[Id].tileData);
     }
-    
+
     IEnumerator CountdownClock()
     {
         while (true)
         {
             CoundownSeconds--;
             yield return new WaitForSeconds(1);
-            
-            if (CoundownSeconds <=1)
+
+            if (CoundownSeconds <= 1)
             {
                 EndDay();
             }
@@ -345,48 +408,91 @@ public class GameState : MonoBehaviour
         }
     }
 
+    public void NoticeControl()
+    {
+        
+
+
+    }
 
 
     IEnumerator NoticeDay()
     {
 
-            int color1 = Random.Range(1,7);
-            int color2 = Random.Range(1,7);
-            
+        //int color1 = (int)Random.Range(1, 7);
+        //int color2 = (int)Random.Range(1, 7);
 
 
-            if(color1 == color2)
+        /*
+         if(color1 == color2)
+         {
+             int tempNum = Random.Range(2, 3);
+             color2 = Mathf.Clamp(color2 + tempNum, 1, 6);
+         }
+         */
+
+
+        bool vaildPair = false;
+        ColorPairs selectedPair = new ColorPairs(0, 0);
+        while (vaildPair == false) 
+        {
+            int address = (int)Random.Range(0, NoticeColors.Length);
+            selectedPair = NoticeColors[address];
+            if (UsedPairs.Count != 0)
             {
-                int tempNum = Random.Range(2, 3);
-                color2 = Mathf.Clamp(color2 + tempNum, 1, 6);
+                foreach (var pair in UsedPairs)
+                {
+                    if (selectedPair.Color1 == pair.Color1 || selectedPair.Color1 == pair.Color2 || selectedPair.Color2 == pair.Color1 || selectedPair.Color2 == pair.Color2)
+                    {
+                        vaildPair = false;
+                        break;
+                    }
+                    else
+                    {
+                        vaildPair = true;
+                    }
+                }
             }
-
-            Notices notices1 = new Notices(color1, color2);
-
-            foreach(var project in projects)
+            else
             {
-                project.swapData(color1, color2);
+                vaildPair = true;
             }
-            notices.Add(notices1);
-            notices1.ChangecolorMeaning();
-            noticeDay += 1;
+            if (vaildPair)
+            {
+                UsedPairs.Add(selectedPair);
+            }
+        }
+
+        //=============================================
+
+        Notices notices1 = new Notices(selectedPair.Color1, selectedPair.Color2);
+
+        foreach (var project in projects)
+        {
+            project.swapData(selectedPair.Color1, selectedPair.Color2);
+        }
+        notices.Add(notices1);
+        notices1.ChangecolorMeaning();
+        noticeDay += 1;
         notices1.NoticeID = notices.Count;
 
-            GameObject Button;
-            Button = Instantiate(Noticedetails, new Vector2(2, 2), Quaternion.identity, NoticePanel.transform);
-            Button.GetComponent<NoticeDetails>().NoticeInfoChange(notices1.C1,notices1.C2,notices1.NoticeID);
-            string tempString = notices1.NoticeMessage;
+        GameObject Button;
+        Button = Instantiate(Noticedetails, new Vector2(2, 2), Quaternion.identity, NoticePanel.transform);
+        Button.GetComponent<NoticeDetails>().NoticeInfoChange(notices1.C1, notices1.C2, notices1.NoticeID);
+        string tempString = notices1.NoticeMessage;
 
-            //updateNoticeText(notices1.NoticeMessage);
-            Events.ChangeNotice(notices1.C1, notices1.C2,tempString);
-            Events.noticeScreen();
+        //updateNoticeText(notices1.NoticeMessage);
+        Events.ChangeNotice(notices1.C1, notices1.C2, tempString);
+        Events.noticeScreen();
 
-            //Debug.Log("next notice day is on " + noticeDay.ToString());
-            //StartCoroutine(Daytimer());
-            yield return new WaitForSeconds(daySeconds + 1f);
+        //Debug.Log("next notice day is on " + noticeDay.ToString());
+        //StartCoroutine(Daytimer());
+              
+
+        yield return new WaitForSeconds(daySeconds + 1f);
     }
 
-    
-    
-    
+
+
+
 }
